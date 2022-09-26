5ec799cde9d549a24c0e3b313de55ec915e2636b
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-09-26 17:32:18 -0500

    re-uploaded directory

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-if not my_list:
-    return None
-    my_list.sort(reverse=True)
-    return my_list[0]
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    largest = 0
+    for index in my_list:
+        if index > largest:
+            largest = index
+    return largest
