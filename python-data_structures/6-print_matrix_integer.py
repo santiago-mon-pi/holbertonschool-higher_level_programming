df27d39f57f4d534887285d8c300d1940036787a
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-09-26 17:27:27 -0500

    re-uploaded directory

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # verify if matrix exists
-    if matrix:
-        for y in range(len(matrix)):
-            for x in range(len(matrix[y])):
-                print(matrix[y][x], end="")
-                if x < len(matrix[y]) - 1:
-                    print(" ", end='')
+    if not matrix:
+        print()
+    else:
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row])):
+                if item != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[row][item]), end=endspace)
             print()
