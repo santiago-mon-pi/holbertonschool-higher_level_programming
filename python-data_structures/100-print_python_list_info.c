fe7d78ab59de4405752b1ef342a0568825b63f87
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-09-26 17:03:56 -0500

    re-uploaded directory

---
+++ python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - checks if a linked list is a palindrome.
+ *@p: placeholder
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	long int len = PyList_Size(p);
+	int i;
+
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", len);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < len; i++)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	}
+}
---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    # verify if matrix exists
+    if matrix:
+        for y in range(len(matrix)):
+            for x in range(len(matrix[y])):
+                print(matrix[y][x], end="")
+                if x < len(matrix[y]) - 1:
+                    print(" ", end='')
+            print()
